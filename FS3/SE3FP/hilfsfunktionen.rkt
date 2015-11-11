c94eb3c5a8f56941b7414679269cb953ca1cc423
Louis Kobras <4kobras@informatik.uni-hamburg.de>
2015-11-11 13:07:01 +0100

    FS3:FP:source doc in hilfsfunktionen

--- FS3/SE3FP/hilfsfunktionen.rkt
+++ FS3/SE3FP/hilfsfunktionen.rkt
@@ -2,6 +2,8 @@
 
 ;;;; Rekursives konkatenieren gleichindizierter Elemente zweier Listen
 ; require length(lst1) == length(lst2)
+; param lst1: Liste, die als Key gemappt werden soll
+; param lst2: Liste mit Werten, die auf die Werte der ersten Liste gemappt werden sollen
 (define (zip lst1 lst2)
   (if (or (empty? lst1)
           (empty? lst2))
