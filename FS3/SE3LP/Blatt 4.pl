3f3b1c5da735a06d344848120c7fd5314087da78
Louis Kobras <4kobras@informatik.uni-hamburg.de>
2015-11-12 12:56:12 +0100

    FS3:LP:HA:W4:a1

--- FS3/SE3LP/Blatt 4.pl
+++ FS3/SE3LP/Blatt 4.pl
@@ -3,3 +3,38 @@
 % AUFGABE 1
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1. ist...
+%% symmetrisch: nein (A ist ein Jahr, B ist eine Person)
+%% reflexiv: nö (ein Jahr kann nicht die Person sein, die in dem Jahr geboren wurde)
+%% transitiv: nope (B ist eine Person und kann damit nicht das Jahr von C sein)
+%% funktional: joa (1:m, wenn X das Geburtsjahr von Y ist und Z das Geburtsjahr von Z ist, dann X=Y, weil man nur in einem Jahr geboren werden kann )
+% 2. ist...
+%% symmetrisch: ja (wenn A neben B ist, ist B auch neben A, zumindest nach der Definition im 3-dimensionalen Raum)
+%% reflexiv: nö (A nimmt den selben Platz ein wie A, kann also nicht neben A sein)
+%% transitiv: nope (A neben B, B neben C, C nicht neben A weil B im Weg)
+%% funktional: nah (n:m, A Nachbar von B und C Nachbar von B, dann nicht A=C, weil B zwei Seiten hat)
+% 3. ist...
+%% symmetrisch: nein (wenn A leichter B, muss B schwerer A sein, somit asymetrisch)
+%% reflexiv: nö (A kann nicht leichter als A sein)
+%% transitiv: jo (wenn A.Masse kleiner B.Masse und B.Masse kleiner C.Masse, dann A.Masse kleiner C.Masse, weil Algebra)
+%% funktional: nah (n:m, da wir vom Gewicht keine Gleicheit ableiten können)
+% 4. ist...
+%% symmetrisch: nein (siehe oben)
+%% reflexiv: japp (A ist kleiner gleich A)
+%% transitiv: jo (wenn A kleiner gleich B und B kleiner gleich C, dann A kleiner gleich C, weil Algebra)
+%% funktional: nah (n:m, da wir von der Größe keine Gleicheit ableiten können)
+% 5. ist...
+%% symmetrisch: ja (wenn A schonmal mit B gespielt hat, hat B auch schonmal mit A gespielt)
+%% reflexiv: japp (A hat schon das ein oder andere Mal mit sich selber in einer Mannschaft gespielt)
+%% transitiv: nope (A und B spielten bei Leverkusen, B und C bei Pauli, A guckt Pauli nicht mit dem Hintern an)
+%% funktional: nah (n:m, weil es mehrere Spieler pro Mannschaft gibt)
+% 6. ist...
+%% symmetrisch: ja (wenn A kongruent B, dann B auch kongruent A)
+%% reflexiv: japp (A ist kongruent zu sich selber)
+%% transitiv: jo (A kongruent B und B kongruent C dann A kongruent B)
+%% funktional: nah (kongruent ja, gleich nicht)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% AUFGABE 2
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
