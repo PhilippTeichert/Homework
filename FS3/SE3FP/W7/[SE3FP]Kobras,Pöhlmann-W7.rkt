c63d9e1555821d42e26a48194f3cf9e89a1c7b9b
Utz Pöhlmann <4poehlma@informatik.uni-hamburg.de>
2015-12-03 22:39:52 +0100

    FP

--- FS3/SE3FP/W7/[SE3FP]Kobras,Pöhlmann-W7.rkt
+++ FS3/SE3FP/W7/[SE3FP]Kobras,Pöhlmann-W7.rkt
@@ -49,17 +49,52 @@
            (step (/ laenge n))]
        (build-list n (lambda (x) (let [(num (* x step))](cons num (f num))))))))
 ;;(cons start (f start))
-
-;;;;Aufgabe 2.1
+ 
+;;;;Aufgabe 2.2
 ;;Gegeben eine Liste an Werten aus Aufgabe 1 ein Intervall als Pair die Werte werden auf das neue intervall abgebildet
 ;;Inklusive der ersten Zahl und inklusive der letzten
-(define (rescaled list interval)
+(define (rescale1d list interval)
   (let [(start (car interval))
         (stop (cdr interval))]
     (let* [(laenge (- stop start))
            (n (length list))
-           (step (/ laenge n))]
+           (step (/ laenge (- n 1)))]
       ;(apply
        ;(lambda (x) (+ x start))
        (build-list n (lambda (x) (+ (* x step) start))))));)
-;;(cons start (f start))
+;(cons start (f start))
+;(rescale1d (range '(0 . 10) 40) '(10 . 50))
+ 
+;;;;Aufgabe 2.2
+;;Gegeben eine Liste an Pairs aus Aufgabe 2.1 und zwei Intervall als Pairs der erste Werte eines Pairs aus der Liste von Pairs wird werden auf das erste intervall abgebildet, der zweite auf das zweite
+;;Inklusive der ersten Zahl und inklusive der letzten
+(define (rescale2d list interval1 interval2)
+  (let [(list1 (map car list))
+        (list2 (map cdr list))]
+    (einslinkseinsrechts
+     (rescale1d list1 interval1)
+     (rescale1d list2 interval2))))
+
+;;;;Hilfsfunktion zur Listenbildung
+;; bildet aus 2 lsiten eine liste aus paaren, wobei das element an stelle x in liste 1 als car des elements an stelle x in der ausgabelsite zu finden ist. für liste 2 analog mit cdr
+(define (einslinkseinsrechts list1 list2)
+  (if (empty? list1)
+      '()
+      (cons
+       (cons (car list1) (car list2))
+       (einslinkseinsrechts (cdr list1) (cdr list2)))))
+      
+  #|(let [(start1 (car interval1))
+        (stop1 (cdr interval1))
+        (start2 (car interval2))
+        (stop2 (cdr interval2))]
+    (let* [(laenge1 (- stop1 start1))
+           (laenge2 (- stop2 start2))
+           (n (length list))
+           (step1 (/ laenge1 (- n 1)))
+           (step2 (/ laenge2 (- n 1)))]
+      ;(apply
+       ;(lambda (x) (+ x start))
+       (build-list '(n . n) (lambda (x) (cons (+ (* (car x) step1) start1) (+ (* (cdr x) step2) start2)))))));)|#
+;(cons start (f start))
+;(rescale2d (function->points sqr '(0 . 10) 5) '(10 . 50) '(5 . 25))
